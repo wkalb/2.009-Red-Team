a7fd8dbd0d2c13b9774f26c36b0802e111c619f2
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2013-11-07 19:23:30 +0100

    Initial catkin commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/2009red/src
+# Build directory: /home/ubuntu/2009red/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ubuntu/2009red/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
